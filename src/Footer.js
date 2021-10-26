9ac4523389c8cb4449f137d8ebe34144f6ce42ff
KLan Stovall <kland13@gmail.com>
2021-10-26 16:36:25 -0700

    lab02

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class Footer extends Component
+{
+    render()
+    {
+        return (
+            <p>K'Lan Stovall</p>
+        )
+    }
+}
+
+export default Footer;
